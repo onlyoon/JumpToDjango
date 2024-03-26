5d1552db1b515b08ac3d54d325a3745a8cd97337
SANGYOON PARK <parkyoon97@gmail.com>
2024-03-26 17:24:01 +0900

    feat: pagination

--- project/mysite/pybo/views.py
+++ project/mysite/pybo/views.py
@@ -3,11 +3,15 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator
 
 
 def index(request):
+    page = request.GET.get('page', '1')
     question_list = Question.objects.order_by('-create_date')
-    context = {'question_list': question_list}
+    paginator = Paginator(question_list, 10)
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
     return render(request, 'pybo/question_list.html', context)
 
 
