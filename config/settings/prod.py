dc8dcc3d1c55734f2ed9a6f167b19d3081e0f511
SANGYOON PARK <parkyoon97@gmail.com>
2024-04-04 18:35:56 +0900

    fix:

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,5 +1,6 @@
 from .base import *
 
+# ALLOWED_HOSTS = ['15.164.143.166', 'parkyoon.xyz']
 ALLOWED_HOSTS = ['15.164.143.166', 'parkyoon.xyz']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
