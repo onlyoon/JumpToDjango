07420d79ab2cfc16f0306d67f300336b13910a9e
SANGYOON PARK <parkyoon97@gmail.com>
2024-04-04 18:36:59 +0900

    fix: the order

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,7 +1,7 @@
 from .base import *
 
 # ALLOWED_HOSTS = ['15.164.143.166', 'parkyoon.xyz']
-ALLOWED_HOSTS = ['15.164.143.166', 'parkyoon.xyz']
+ALLOWED_HOSTS = ['parkyoon.xyz', '15.164.143.166']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
